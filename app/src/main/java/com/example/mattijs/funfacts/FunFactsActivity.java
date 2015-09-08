5826a1f9f8287237c9eb8501d16624442ddb8344
Mattijs Meiboom <mattijsmeiboom@gmail.com>
2015-09-08 23:18:47 +0200

    button

--- app/src/main/java/com/example/mattijs/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/mattijs/funfacts/FunFactsActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
@@ -16,8 +19,33 @@ public class FunFactsActivity extends Activity {
         setContentView(R.layout.activity_fun_facts);
 
         // Declare our View variables
-        TextView factlabel;
-        Button showFactButton;
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] facts = {
+                        "one",
+                        "two",
+                        "three"
+                };
+
+                Random random = new Random();
+                int number = random.nextInt(facts.length);
+
+                String previous = (String) factLabel.getText();
+                while(previous.equals(facts[number])) {
+                    number = random.nextInt(facts.length);
+                }
+
+
+                factLabel.setText(facts[number]);
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
     }
 
 
